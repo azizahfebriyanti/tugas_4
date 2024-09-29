7f1dc0d216bde6b7da0bfe2365c97796a4179b11
azizahfebriyanti <azizahfebriyanti20@gmail.com>
2024-09-29 09:52:45 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Landing Page - Guestbook</title>
+</head>
+<body>
+    <h1>Welcome to The Guestbook!</h1>
+    <p>Selamat datang di situs buku tamu kami. Silakan mengisi buku tamu atau melihat daftar tamu yang telah mengisi.</p>
+
+    <ul>
+        <li><a href="input.php">Fill The Guestbook</a></li>
+        <li><a href="tabel.php">See the Guestlist</a></li>
+    </ul>
+</body>
+</html>
